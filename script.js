1485e777b94eefd988610400da68b0e140694c25
Holulosamo <eliasrodriguez000@gmail.com>
2021-09-11 00:26:32 -0300

    Changing navbar styles

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const d = document,
-    menuBtn = d.querySelector('.menu-button'),
-    navBar = d.querySelector('.nav');
+    $menuBtn = d.querySelector('.menu-button'),
+    $navBar = d.querySelector('.nav'),
+    $menuItem = d.querySelectorAll('.menu__link');
 
-menuBtn.addEventListener('click', (e) =>{
-    navBar.classList.toggle("nav-show");
+$menuBtn.addEventListener('click', (e) =>{
+    $navBar.classList.toggle("nav-show");
+    $menuItem.forEach(el => {
+        el.classList.toggle("menu__link-show");
+    })
 });
