294acfc150ec4a62315d8f121d37344e52394fbe
Holulosamo <eliasrodriguez000@gmail.com>
2021-09-23 20:29:27 -0300

    adding styles to cards

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const d = document,
     $navBar = d.querySelector('.nav'),
     $menuItem = d.querySelectorAll('.menu__link');
 
-$menuBtn.addEventListener('click', (e) =>{
-    $navBar.classList.toggle("nav-show");
-    $menuItem.forEach(el => {
-        el.classList.toggle("menu__link-show");
-    })
+d.addEventListener('click', (e) =>{
+    if(e.target.matches(".button") || e.target.matches(".menu-button")){
+        $navBar.classList.toggle("nav-show");
+        $menuItem.forEach((el) => {
+          el.classList.toggle("menu__link-show");
+        });
+    }
+
+    if(e.target.matches(".menu__link") && $navBar.classList.contains('nav-show')){
+        $navBar.classList.remove('nav-show');
+        $menuItem.forEach((el) => {
+          el.classList.remove("menu__link-show");
+        });
+    }
 });
+
